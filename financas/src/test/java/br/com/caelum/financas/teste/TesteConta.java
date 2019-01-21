f33d883cbf952f2c26d8d335873d02e659eb61e6
Roberto Henrique Salami <robertohsalami@gmail.com>
2019-01-21 16:20:05 -0200

    Inicio da Aula 3 do curso Java e JPA: Persista seus objetos com JPA2 e Hibertnate

--- financas/src/test/java/br/com/caelum/financas/teste/TesteConta.java
+++ financas/src/test/java/br/com/caelum/financas/teste/TesteConta.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
 
 public class TesteConta {
 	
@@ -16,15 +17,13 @@ public class TesteConta {
 		conta.setNumero("456");
 		
 		//Chamar a JPA para de fato persistir essa entidade
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas");
-		EntityManager em = emf.createEntityManager();
+		EntityManager em = new JPAUtil().getEntityManager();
 		
 		em.getTransaction().begin();
 		em.persist(conta);
 		em.getTransaction().commit();
 		
 		em.close();
-		emf.close();
 		
 	}
 
